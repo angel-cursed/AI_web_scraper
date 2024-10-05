1d1e56fda6aa2f8b16cfd040d4369708d2ed81d1
angel-cursed <130891386+angel-cursed@users.noreply.github.com>
2024-10-05 15:11:00 +0200

    Update web scraper application:    - Add functionality to parse specific information requested by user.    - Enhance HTML parsing with BeautifulSoup.    - Extract body content and clean it before further processing.    - Split DOM content for better parsing efficiency.    - Implement OLLAMA language model for data extraction.

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import streamlit as st
-from scrape import scrape_website
+from scrape import scrape_website, split_dom_content, clean_body_content, extract_body_content
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
 
 if st.button("Scrape Site"):
     st.write("Scraping the Website")
+
     result = scrape_website(url)
-    print(result)
+    body = extract_body_content(result)
+    cleaned_body = clean_body_content(body)
+
+    st.session_state.dom_content = cleaned_body
+
+    with st.expander("View DOM Content"):
+        st.text_area("DOM Content", cleaned_body, height=300)
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse.")
+
+    if st.button("Parse Content") and parse_description:
+        st.write("Parsing the Content")
+
+        dom_chunks = split_dom_content(st.session_state.dom_content)
+        result = parse_with_ollama(dom_chunks, parse_description)
+        st.write(result)
--- scrape.py
+++ scrape.py
@@ -1,4 +1,5 @@
 import selenium.webdriver as webdriver
+from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.service import Service
 def scrape_website(website):
     print("Launching chrome browser")
@@ -18,3 +19,27 @@ def scrape_website(website):
         return html
     finally:
         driver.quit()
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body = soup.body
+    if body:
+        return str(body)
+    return ""
+
+def clean_body_content(body):
+    soup = BeautifulSoup(body, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")
+
+    cleaned_content = "\n".join(line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length = 6000):
+    return [
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
